b264aaf218c4400a99099dabfd61a7308cde8f5d
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-21 14:11:14 +0200

    add signup component

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignupService } from 'src/app/services/auth/signup.service';
+import { SignupRequest } from './signup-request';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupRequest !: SignupRequest;
+  signupForm !: FormGroup;
+  
+  constructor(private signupService: SignupService) {
+    this.signupRequest = {
+      email: '',
+      username: '',
+      password: ''
+    };
+  }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  signup(): void {
+    this.signupRequest.email = this.signupForm.get('email')?.value;
+    this.signupRequest.username = this.signupForm.get('username')?.value;
+    this.signupRequest.password = this.signupForm.get('password')?.value;
+    
+    this.signupService.signup(this.signupRequest).subscribe(data => console.log(data));
+  }
+}
