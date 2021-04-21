2e116fb830e8c2b1a66f38561b5fb1bf353e670f
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-21 14:43:52 +0200

    add barebones of login component (no logic yet)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm !: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', Validators.required)
+    })
   }
 
+  login() {
+    
+  }
 }
