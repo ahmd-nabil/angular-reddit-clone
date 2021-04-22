99cf744a7015b9e6f242fa5c0068a21ed6d39fe0
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-22 10:45:06 +0200

    add token interceptor for every request

---
+++ src/app/services/auth/token-interceptor.service.ts
@@ -0,0 +1,27 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor{
+
+  constructor(private localStorage: LocalStorageService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const jwt = this.localStorage.retrieve('authorization');
+    if(jwt) {
+      this.addJwtToHeader(req, jwt);
+    }
+    return next.handle(req);
+  }
+
+  addJwtToHeader(req: HttpRequest<any>, jwt: any) {
+    return req.clone(
+      {headers: req.headers.set('Authorization', jwt)}
+    )
+  }
+
+}
