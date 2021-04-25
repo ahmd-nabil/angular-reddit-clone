6415a04751751852b64585a0beec6131663fd07b
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-25 13:22:47 +0200

    finishing subreddits-side-bar

--- src/app/subreddit-side-bar/subreddit-side-bar.component.ts
+++ src/app/subreddit-side-bar/subreddit-side-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Post } from '../model/post';
+import { map } from 'rxjs/operators';
 import { Subreddit } from '../model/subreddit';
+import { SubredditService } from '../services/subreddit.service';
 
 @Component({
   selector: 'app-subreddit-side-bar',
@@ -8,11 +9,22 @@ import { Subreddit } from '../model/subreddit';
   styleUrls: ['./subreddit-side-bar.component.css']
 })
 export class SubredditSideBarComponent implements OnInit {
+  
   subreddits !: Array<Subreddit>;
-  displayViewAll !: boolean;
-  constructor() { }
+  displayViewMore !: boolean;
+  constructor(private subredditService: SubredditService) { }
 
   ngOnInit(): void {
+    this.subredditService.getAllSubreddits().subscribe(
+      data => {
+        if(data.length <= 4) {
+          this.subreddits = data;
+        } else {
+          this.subreddits = data.splice(0, 4);
+          this.displayViewMore = true;
+        }
+         this.subreddits.map(subreddit => subreddit.name = `r/${subreddit.name}`);
+      });
   }
 
 }
